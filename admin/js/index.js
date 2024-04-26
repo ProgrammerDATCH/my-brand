9a8c10db812eeba9a3e908841c0a6213bf143e85
ProgrammerDATCH <programmerdatch@gmail.com>
2024-04-26 12:36:00 +0200

    Added Admin Dashboard

---
+++ admin/js/index.js
@@ -0,0 +1,93 @@
+const serverLink = 'https://mybrandbackend-4e8h.onrender.com/api';
+
+const blogsCount = document.getElementById("blogsCount")
+const suggestionsCount = document.getElementById("suggestionsCount")
+const usersCount = document.getElementById("usersCount")
+
+const showDashboard = async () => {
+    const res = await fetch(`${serverLink}/admin/dashboard`, {
+        method: "GET",
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${getCookie()}`
+        }
+    });
+    if (!res.ok) {
+        console.log("Send API Failed!");
+        return;
+    }
+    const result = await res.json();
+    const dashboard = result.message;
+    blogsCount.innerText = dashboard.blogs;
+    suggestionsCount.innerText = dashboard.suggestions;
+    usersCount.innerText = dashboard.users;
+}
+
+
+
+const allSuggestions = document.getElementById("allSuggestions");
+
+const showSuggestion = async () => {
+    const res = await fetch(`${serverLink}/suggestion/suggestions`, {
+        method: "GET",
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${getCookie()}`
+        }
+    });
+    if (!res.ok) {
+        console.log("Send API Failed!");
+        return;
+    }
+    const suggestions = await res.json();
+    allSuggestions.innerHTML = ""
+    suggestions.message.forEach(suggestion => {
+        allSuggestions.innerHTML += `
+        <tr>
+            <td>${suggestion._id}</td>
+            <td>${suggestion.name}</td>
+            <td>${suggestion.email}</td>
+            <td>${suggestion.subject}</td>
+            <td>${suggestion.message}</td>
+            <td class="action-btns"><button class="delete-btn" onclick="deleteSuggestion('${suggestion._id}')"><i class="fa fa-trash"></i> Delete</button></td>
+        </tr>
+                `
+    });
+}
+
+function getCookie() {
+    const cookies = document.cookie.split(";").map(cookie => cookie.trim());
+    for (const cookie of cookies) {
+        const [cookieName, cookieValue] = cookie.split("=");
+        if (cookieName === "adminToken") {
+            return cookieValue;
+        }
+    }
+    return null;
+}
+
+
+
+const deleteSuggestion = async(id) => {
+    const res = await fetch(`${serverLink}/suggestion/delete`, {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${getCookie()}`
+        },
+        body: JSON.stringify({id})
+    });
+    if (!res.ok) {
+        console.log("Send API Failed!");
+        return;
+    }
+    const result = await res.json();
+    
+    alert(result.message)
+    showSuggestion();
+}
+
+
+
+showDashboard();
+showSuggestion();
